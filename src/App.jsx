108a7f8c190299b7e264b8575c54e2759f86e316
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-14 08:34:03 +0700

    initial commit & set up project with tailwind and react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-slate-600 text-3xl">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
