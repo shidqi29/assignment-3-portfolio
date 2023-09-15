502073ee8bd151c2816fd9a337570f90d6e28ad7
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-15 14:11:19 +0700

    feat[Home]: add content at Home Page and styling Page Layout

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -2,18 +2,20 @@ import PropTypes from "prop-types";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const PageLayout = ({children}) => {
-    return (
-        <>
-        <Navbar />
-        <main className="min-h-screen">{children}</main>
-        <Footer />
-        </>
-    );
-}
+const PageLayout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main className="container mx-auto flex min-h-screen w-full justify-center items-center">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 PageLayout.propTypes = {
-    children: PropTypes.node
-}
- 
+  children: PropTypes.element,
+};
+
 export default PageLayout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
-}
- 
+  return (
+    <div className="-mt-20 flex flex-col items-center justify-center gap-10 md:flex-row">
+      <div className="h-64 w-64">
+        <img src="profile.jpg" alt="" className="h-full w-full rounded-full" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-xl">Hi, I am</p>
+        <h1 className="text-3xl font-bold tracking-wide">Shidqi Amirul Haq</h1>
+        <p className="text-2xl tracking-widest">a Front-End Developer</p>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
