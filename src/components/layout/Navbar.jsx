1311cbf3048faf3c75d9fc92c9e88da75b529636
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-15 13:36:09 +0700

    feat[Navbar]: add NavItem & styling Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,29 @@
+import { NavLink } from "react-router-dom";
+import { NavItem } from "../../constant/NavItem";
+
 const Navbar = () => {
-    return (
-        <header>
-            <nav>ts</nav>
-        </header>
-    );
-}
- 
+  return (
+    <header className="container mx-auto mt-2 flex justify-center">
+      <nav className="bg-secondary top-0 mx-auto rounded-full px-8 py-4 hover:shadow-xl ">
+        <ul className="flex space-x-8">
+          {NavItem.map((item, index) => (
+            <li key={index}>
+              <NavLink
+                to={item.path}
+                style={({ isActive }) => ({
+                  borderBottom: isActive ? "3px solid #3f5fca" : "transparent",
+                  fontWeight: isActive ? "bold" : "normal",
+                  transition: "all 0.2s",
+                })}
+              >
+                {item.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 export default Navbar;
---
+++ src/constant/NavItem.jsx
@@ -0,0 +1,26 @@
+export const NavItem = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "About",
+        path: "/about"
+    },
+    {
+        title: "Experiences",
+        path: "/experiences"
+    },
+    {
+        title: "Skills",
+        path: "/skills"
+    },
+    {
+        title: "Interests",
+        path: "/interests"
+    },
+    {
+        title: "Awards",
+        path: "/awards"
+    },
+]
