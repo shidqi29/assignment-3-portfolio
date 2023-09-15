76880a4754f56e6f1aa0c9e1edae912f8c05075c
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-15 10:53:57 +0700

    style: add color pallete and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#738bd9",
+        secondary: "#30280d",
+        accent: "#3f5fca",
+      }
+    },
   },
   plugins: [],
 };
