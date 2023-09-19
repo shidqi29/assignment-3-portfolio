c331cfe6d8c058e9058794a4333a787e0b3ef7d8
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-19 22:36:07 +0700

    feat: add footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
-        <footer>
-            <div>tesawda</div>
+        <footer className="flex justify-center bg-secondary">
+            <div className="py-4">
+                <p>Made with ❤️ by <strong>Shidqi Amirul Haq</strong></p>
+            </div>
         </footer>
     );
 }
