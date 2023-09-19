20fa8702f7714454e75fd0514ee8c47ef068439b
shidqi29 <shidqiamirulhaq@gmail.com>
2023-09-19 22:42:17 +0700

    style[footer]: fix margin for footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,13 @@
 const Footer = () => {
-    return (
-        <footer className="flex justify-center bg-secondary">
-            <div className="py-4">
-                <p>Made with ❤️ by <strong>Shidqi Amirul Haq</strong></p>
-            </div>
-        </footer>
-    );
-}
- 
+  return (
+    <footer className="mt-10 flex justify-center bg-secondary md:mt-5 lg:mt-0">
+      <div className="py-4">
+        <p>
+          Made with ❤️ by <strong>Shidqi Amirul Haq</strong>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
